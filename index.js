a191b712ac07e8772b2c6c73bec4ffdd79484c49
yusecase <yusecasegit.gmail.com>
2017-10-05 13:06:40 +0000

    日付ログを削除

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const http = require('http');
 const jade = require('jade');
 const server = http.createServer((req, res) => {
-  const now = new Date();
-  console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
+
+  console.info('Requested by ' + req.connection.remoteAddress);
   res.writeHead(200, {
     'Content-Type': 'text/html; charset=utf-8'
   });
@@ -38,7 +38,7 @@ const server = http.createServer((req, res) => {
       }).on('end', () => {
         body = Buffer.concat(body).toString();
         const decoded = decodeURIComponent(body);
-        console.info('[' + now + '] 投稿: ' + decoded);
+        console.info('投稿: ' + decoded);
         res.write('<!DOCTYPE html><html lang="ja"><head><meta charset="utf-8"></head><body><h1>' +
           decoded + 'が投稿されました</h1></body></html>');
         res.end();
@@ -55,5 +55,5 @@ const server = http.createServer((req, res) => {
 });
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
-  console.info('[' + new Date() + '] Listening on ' + port);
+  console.info('Listening on ' + port);
 });
